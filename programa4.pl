6f5e695f625594df3d35704272d9703a93d367f6
mageMerlin8 <emilio.1997@gmail.com>
2019-01-23 12:32:03 -0600

    saludos

---
+++ programa4.pl
@@ -0,0 +1,12 @@
+saludos :-
+    prolg_pais(Pais),
+    write("Hola "),
+    write(Pais),nl,
+    fail.
+saludos.
+prolg_pais(japon).
+prolg_pais(francia).
+prolg_pais(hungria).
+prolg_pais(bhutan).
+prolg_pais(colombia).
+prolg_pais(espania).
