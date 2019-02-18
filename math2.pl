8aef79396d2222c1ae8c2dde11f3218328c7847a
emilio mena <magemerlin8@pop-os.localdomain>
2019-02-18 11:26:09 -0600

    send+more=money

---
+++ math2.pl
@@ -0,0 +1,32 @@
+
+numero(0).
+numero(1).
+numero(2).
+numero(3).
+numero(4).
+numero(5).
+numero(6).
+numero(7).
+numero(8).
+numero(9).
+/*
+numero(S),
+numero(E),
+numero(N),
+numero(D),
+numero(M),
+numero(O),
+numero(R),
+numero(Y),
+M=\=0,
+S=\=E,S=\=N,S=\=D,S=\=M,S=\=O,S=\=R,S=\=Y,
+      E=\=N,E=\=D,E=\=M,E=\=O,E=\=R,E=\=Y,
+            N=\=D,N=\=M,N=\=O,N=\=R,N=\=Y,
+                  D=\=M,D=\=O,D=\=R,D=\=Y,
+                        M=\=O,M=\=R,M=\=Y,
+                              O=\=R,O=\=Y,
+                                    R=\=Y,
+   S*1000+E*100+N*10+D
+  +M*1000+O*100+R*10+E
+=:=M*10000+O*1000+N*100+E*10+Y.
+      /*
